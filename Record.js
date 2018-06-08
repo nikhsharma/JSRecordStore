6241c881314967d7056f3a878d83fd862b724d77
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-06-08 22:04:33 +0100

    Can print Record details as a string

--- Record.js
+++ Record.js
@@ -5,4 +5,8 @@ const Record = function(artist, title, genre, price) {
   this.price = price;
 }
 
+Record.prototype.displayDetails = function() {
+  return ('Artist: ' + this.artist + ', Title: ' + this.title + ', Genre: ' + this.genre + ', Price: ' + this.price);
+}
+
 module.exports = Record;
--- specs/record_spec.js
+++ specs/record_spec.js
@@ -6,7 +6,7 @@ describe('Record', function() {
   let record;
 
   beforeEach(function() {
-    record = new Record('Courtney Barnett', 'Tell Me How You Really Feel', 'alternative', 15.99);
+    record = new Record('Courtney Barnett', 'Tell Me How You Really Feel', 'Alternative', 15.99);
   });
 
   it('should have an artist', function() {
@@ -18,11 +18,14 @@ describe('Record', function() {
   });
 
   it('should have a genre', function() {
-    assert.strictEqual(record.genre, 'alternative');
+    assert.strictEqual(record.genre, 'Alternative');
   });
 
   it('should have a price', function() {
     assert.strictEqual(record.price, 15.99);
   })
 
+  it('should print all details as a string', function() {
+    assert.strictEqual(record.displayDetails(), 'Artist: Courtney Barnett, Title: Tell Me How You Really Feel, Genre: Alternative, Price: 15.99');
+  });
 });
